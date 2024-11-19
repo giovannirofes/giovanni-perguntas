5b6f39b4f762b6eea1475b62d718d0d618ed5b1b
giovannirofes <giovannirofes@gmail.com>
2024-11-19 14:24:46 -0300

    perguntas
    .

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Java?',
+    'O Java é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é uma função?',
+    'Uma função é uma regra que relaciona cada elemento de um conjunto a um único elemento de outro. '
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz olá em Inglês?',
+    'Oi em ingles é Hello (rélou)'
+)
